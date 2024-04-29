cbd775ca586c1ae68133b555030ea61e2821e89f
khaoula <khaoulahouda140@gmail.com>
2024-04-29 19:54:03 +0100

    j ai ajoute le controller DetailPrestController afin de gerer the views in DetailPrest directory

---
+++ ProjetPfa/Controllers/DetailPrestController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetPfa.Models;
+using ProjetPfa.ModelView;
+using System;
+
+
+
+namespace ProjetPfa.Controllers
+{
+    public class DetailPrestController : Controller
+    {
+        private readonly MyContext context;
+
+        public DetailPrestController(MyContext context)
+        {
+            context = context;
+        }
+      
+        [HttpPost]
+        public ActionResult Create(DetailCompteVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                var detailCompte = new DetailCompte
+                {
+                    DisplayName = model.DisplayName,
+                    Description = model.Description,
+                    OrgName = model.OrgName,
+                    Languages = new List<Language>() 
+                };
+
+               
+                context.DetailComptes.Add(detailCompte);
+                context.SaveChanges();
+
+               
+                if (model.Languages != null && model.LanguageLevels != null && model.Languages.Count == model.LanguageLevels.Count)
+                {
+                    for (int i = 0; i < model.Languages.Count; i++)
+                    {
+                        var language = new Language
+                        {
+                            Name = model.Languages[i],
+                            Level = model.LanguageLevels[i],
+                            DetailCompteId = detailCompte.Id 
+                        };
+                        context.Languages.Add(language);
+                    }
+                    context.SaveChanges();
+                }
+
+               
+                return RedirectToAction("SuccessPage");
+            }
+
+          
+            return View(model);
+        }
+        public ActionResult SuccessPage()
+        {
+          
+            return View();
+        }
+        public ActionResult social()
+        {
+
+            return View();
+        }
+        public ActionResult verify()
+        {
+
+            return View();
+        }
+    }
+}
+
+
